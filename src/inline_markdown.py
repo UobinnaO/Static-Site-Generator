2f8a2ffb482c6cd467c6aa50f6f4fa49f29362f1
uobinnao <uobinnao@gmail.com>
2025-05-18 00:46:31 -0400

    test

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,57 +1,90 @@
-# from textnode import TextNode, TextType
+from textnode import TextNode, TextType
+import re
 
-# def split_nodes_delimiter(old_nodes, delimiter, text_type):
-#     rtn_lst_nodes = list(filter(lambda x: x.text_type != TextType.TEXT, old_nodes))
-#     only_texttype_node = list(filter(lambda x: x.text_type == TextType.TEXT, old_nodes))
-#     node_text_lst = list(map(lambda x: x.text, only_texttype_node))
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    rtn_lst_nodes = list(filter(lambda x: x.text_type != TextType.TEXT, old_nodes))
+    only_texttype_node = list(filter(lambda x: x.text_type == TextType.TEXT, old_nodes))
+    node_text_lst = list(map(lambda x: x.text, only_texttype_node))
 
-#     word_start_end_locations = []
+    word_start_end_locations = []
 
-#     def locate_delimiter(str, delimiter, str_not_checked):
-#         single_delimiter_location = str.find(delimiter)
-#         word_locations_lst_len = len(word_start_end_locations)
-#         if delimiter not in str:
-#             return
-#         single_word_location = 0
-#         if word_locations_lst_len % 2 == 0:
-#             single_word_location = (
-#                 str.find(delimiter) + len(str_not_checked) + len(delimiter)
-#             )
-#         if word_locations_lst_len % 2 != 0:
-#             single_word_location = (
-#                 str.find(delimiter)
-#                 + len(delimiter)
-#                 + len(str_not_checked)
-#                 - len(delimiter)
-#             )
-#         str_not_checked = (
-#             str_not_checked + str[: single_delimiter_location + len(delimiter)]
-#         )
-#         word_start_end_locations.append(single_word_location)
-#         return locate_delimiter(
-#             str[single_delimiter_location + len(delimiter) :],
-#             delimiter,
-#             str_not_checked,
-#         )
+    def locate_delimiter(str, delimiter, str_not_checked):
+        single_delimiter_location = str.find(delimiter)
+        word_locations_lst_len = len(word_start_end_locations)
+        if delimiter not in str:
+            return
+        single_word_location = 0
+        if word_locations_lst_len % 2 == 0:
+            single_word_location = (
+                str.find(delimiter) + len(str_not_checked) + len(delimiter)
+            )
+        if word_locations_lst_len % 2 != 0:
+            single_word_location = (
+                str.find(delimiter)
+                + len(delimiter)
+                + len(str_not_checked)
+                - len(delimiter)
+            )
+        str_not_checked = (
+            str_not_checked + str[: single_delimiter_location + len(delimiter)]
+        )
+        word_start_end_locations.append(single_word_location)
+        return locate_delimiter(
+            str[single_delimiter_location + len(delimiter) :],
+            delimiter,
+            str_not_checked,
+        )
 
-#     for text in node_text_lst:
-#         locate_delimiter(text, delimiter, "")
-#         location_pairs = list(
-#             zip(word_start_end_locations[::2], word_start_end_locations[1::2])
-#         )
-#         emphasized_phrases = []
-#         for pair in location_pairs:
-#             emphasized_phrases.append(text[pair[0] : pair[1]])
-#         print(f"{emphasized_phrases}\n")
+    for text in node_text_lst:
+        locate_delimiter(text, delimiter, "")
+        location_pairs = list(
+            zip(word_start_end_locations[::2], word_start_end_locations[1::2])
+        )
+        emphasized_phrases = []
+        for pair in location_pairs:
+            emphasized_phrases.append(text[pair[0] : pair[1]])
+        print(f"{emphasized_phrases}\n")
 
-#         text_split = text.split(delimiter)
-#         if "" in text_split:
-#             text_split.remove("")
+        text_split = text.split(delimiter)
+        if "" in text_split:
+            text_split.remove("")
 
-#         for text in text_split:
-#             if text in emphasized_phrases:
-#                 rtn_lst_nodes.append(TextNode(text, text_type))
-#             else:
-#                 text_type2 = TextType.TEXT
-#                 rtn_lst_nodes.append(TextNode(text, text_type2))
-#     return rtn_lst_nodes
+        for text in text_split:
+            if text in emphasized_phrases:
+                rtn_lst_nodes.append(TextNode(text, text_type))
+            else:
+                text_type2 = TextType.TEXT
+                rtn_lst_nodes.append(TextNode(text, text_type2))
+    return rtn_lst_nodes
+
+def extract_markdown_images(text):
+    """takes raw markdown text and returns a list of tuples.
+    Each tuple should contain the alt text and the URL of any markdown
+    images
+
+
+    Args:
+        text (_type_): long text
+
+    Returns:
+        _type_: list of tuple(alt text, img url)
+    """
+
+    alt_text = re.findall(r"\[(.*?)\]", text)
+    img_url = re.findall(r"\((.*?)\)", text)
+    return list(zip(alt_text, img_url))
+
+def extract_markdown_links(text):
+    """takes raw markdown text and returns a list of tuples.
+    Each tuple should contain the anchor text and the URL of any markdown
+    images
+
+    Args:
+        text (_type_): long text
+
+    Returns:
+        _type_: list of tuple(anchor text, link url)
+    """
+    anchor_text = re.findall(r"\[(.*?)\]", text)
+    link_url = re.findall(r"\((.*?)\)", text)
+    return list(zip(anchor_text, link_url))
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType, text_node_to_html_node, split_nodes_delimiter
+from textnode import TextNode, TextType, text_node_to_html_node, split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
 
 class TestTextNode(unittest.TestCase):
